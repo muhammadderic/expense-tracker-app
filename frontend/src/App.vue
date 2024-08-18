33cabd66744393dbc96a43817f1a30c452b5f3e0
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-18 08:41:51 +0700

    Add handle transaction submit and install vue toastification

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,46 @@
 <script setup>
-  import AddTransaction from './components/AddTransaction.vue';
+import AddTransaction from './components/AddTransaction.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import MainBalance from './components/MainBalance.vue';
-  import MainHeader from './components/MainHeader.vue'
+import MainHeader from './components/MainHeader.vue'
 import TransactionsList from './components/TransactionsList.vue';
+
+import { ref, onMounted } from 'vue';
+import {useToast} from 'vue-toastification';
+
+const transactions = ref([]);
+const toast = useToast();
+
+// Mounted transactions value from local storage
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+})
+
+const handleTransactionsSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  })
+
+  saveTransactionsToLocalStorage();
+
+  toast.success("Transaction added successfully");
+}
+
+// Generate a random id
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+};
+
+// Save transactions to local storage
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
 </script>
 
 <template>
@@ -11,11 +48,7 @@ import TransactionsList from './components/TransactionsList.vue';
   <div class="container">
     <MainBalance :total="100"/>
     <IncomeExpenses :income="100.00" :expense="50.00"/>
-    <TransactionsList :transactions="[
-      { id: 1, text: 'Salary', amount: 500, },
-      { id: 2, text: 'Book', amount: -10, },
-      { id: 3, text: 'Camera', amount: -150 }
-    ]"/>
-    <AddTransaction/>
+    <TransactionsList :transactions="transactions"/>
+    <AddTransaction @transactionSubmitted="handleTransactionsSubmitted"/>
   </div>
 </template>
--- frontend/src/components/AddTransaction.vue
+++ frontend/src/components/AddTransaction.vue
@@ -1,9 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
+
+const text = ref("");
+const amount = ref("");
+
+const toast = useToast();
+const emit = defineEmits(["transactionSubmitted"]);
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    toast.error("Both fields must be filled");
+    return;
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  }
+
+  emit("transactionSubmitted", transactionData);
+
+  text.value = "";
+  amount.value = "";
+}
+</script>
+
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form 
+    id="form"
+    @submit.prevent="onSubmit"
+  >
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input 
+        type="text" 
+        id="text" 
+        placeholder="Enter text..." 
+        v-model="text"
+      />
     </div>
 
     <div class="form-control">
@@ -11,7 +47,12 @@
         Amount <br>
         (negative - expense, positive - income)
       </label>
-      <input type="text" id="amount" placeholder="Enter amount..." />
+      <input 
+        type="text" 
+        id="amount" 
+        placeholder="Enter amount..." 
+        v-model="amount"
+      />
     </div>
 
     <button class="btn">Add transaction</button>
