14d685360a819f158a9c78c106645cb2ff969fa3
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-18 08:33:35 +0700

    Create a component to show a list of transactions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
 import MainBalance from './components/MainBalance.vue';
   import MainHeader from './components/MainHeader.vue'
+import TransactionsList from './components/TransactionsList.vue';
 </script>
 
 <template>
@@ -9,5 +10,10 @@ import MainBalance from './components/MainBalance.vue';
   <div class="container">
     <MainBalance :total="100"/>
     <IncomeExpenses :income="100.00" :expense="50.00"/>
+    <TransactionsList :transactions="[
+      { id: 1, text: 'Salary', amount: 500, },
+      { id: 2, text: 'Book', amount: -10, },
+      { id: 3, text: 'Camera', amount: -150 }
+    ]"/>
   </div>
 </template>
---
+++ frontend/src/components/TransactionsList.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li 
+      v-for="transaction in props.transactions" 
+      :key="transaction.id"
+      :class="transaction.amount > 0 ? 'plus' : 'minus'"
+    >
+      {{ transaction.text }}
+      <span>
+        ${{ transaction.amount }}
+      </span>
+      <button class="delete-btn">
+        x
+      </button>
+    </li>
+  </ul>
+</template>
