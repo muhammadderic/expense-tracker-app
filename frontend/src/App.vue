0d71c7b1981fa48747b2b421e34ba18346805473
muhammadderic <muhammad.donny.e@gmail.com>
2024-08-18 08:22:36 +0700

    Add main balance component to show the total balance

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+  import MainBalance from './components/MainBalance.vue';
   import MainHeader from './components/MainHeader.vue'
 </script>
 
 <template>
   <MainHeader/>  
+  <div class="container">
+    <MainBalance :total="100"/>
+  </div>
 </template>
---
+++ frontend/src/components/MainBalance.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <h4>Your Balance</h4>
+  <h1 id="balance">$ {{ props.total }}</h1>
+</template>
